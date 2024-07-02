fad35843e1464d4f4172ac85852993091f763997
Carla Gatica <carla.gatica@gmail.com>
2024-07-01 22:43:19 -0300

    registro usuarios funcionando

---
+++ php/register.php
@@ -0,0 +1,30 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$database_name="usersdb";
+
+$conn=mysqli_connect($servername,$username,$password,$database_name);
+if (!$conn) {
+    die("Coneccion fallida:" . mysqli_connect_error());
+}
+
+if(isset($_POST['register'])) {
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $fechaNacimiento = $_POST['fechaNacimiento'];
+    $pais = $_POST['pais'];
+
+    $sql_query = "INSERT INTO users (nombre, apellido, email, pass, fechaNacimiento, pais) VALUES ('$nombre', '$apellido', '$email', '$pass', '$fechaNacimiento', '$pais')";
+
+    if(mysqli_query($conn, $sql_query)) {
+        echo "Usuario generado exitosamente";
+    }
+    else {
+        echo "Error: " . $sql . "" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+?>
